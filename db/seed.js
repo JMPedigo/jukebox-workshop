4740df8da13e8b6c024f6fe5dc66cd909c371d86
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-04 09:06:17 -0500

    Adds seed data for playlists

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,20 @@
 import db from "#db/client";
+import { faker } from "@faker-js/faker";
 
+import { createPlaylist } from "./queries/playlist";
 await db.connect();
 await seed();
 await db.end();
 console.log("🌱 Database seeded.");
 
+/** seed the database with:
+ * at least 20 tracks and 10 playlists.
+ * Create at least 15 playlists_tracks so that some of the seeded tracks belong to some of the seeded playlists. */
 async function seed() {
-  // TODO
+  for (let i = 1; i <= 15; i++) {
+    await createPlaylist({
+      name: faker.music.album(),
+      description: faker.music.genre(),
+    });
+  }
 }
