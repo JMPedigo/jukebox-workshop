8e7301ba0f71d2057c657cfc7df9db938f700879
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-04 16:05:00 -0500

    Changes default to async in order to allow query

--- db/queries/playlists_tracks.js
+++ db/queries/playlists_tracks.js
@@ -1,13 +1,15 @@
 import db from "#db/client";
 
-export default function createPlaylistTrack(playlistId, trackId) {
-  const sql =`
+export async function createPlaylistTrack(playlistId, trackId) {
+  const sql = `
   INSERT INTO playlists_tracks
     (playlist_id, track_id)
   VALUES
     ($1, $2)
   RETURNING *
   `;
-  const { rows: [playlistTrack], } = await db.query(sql, [playlistId, trackId]);
+  const {
+    rows: [playlistTrack],
+  } = await db.query(sql, [playlistId, trackId]);
   return playlistTrack;
 }
