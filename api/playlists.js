cbdb97fe9687aeb9c9a7e03d325bb71b9166f6e9
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-04 17:50:34 -0500

    Adds routing middleware for POST /playlists/:id/tracks

--- api/playlists.js
+++ api/playlists.js
@@ -8,6 +8,8 @@ import {
   getPlaylistById,
   getPlaylists,
 } from "#db/queries/playlists";
+import { getTracksByPlaylistId } from "#db/queries/tracks";
+import { createPlaylistTrack } from "#db/queries/playlists_tracks";
 
 /** Routing middleware for GET /playlists */
 router.get("/", async (req, res) => {
@@ -43,4 +45,24 @@ router.param("id", async (req, res, next, id) => {
 /** Routing middleware for GET /playlists/:id */
 router.get("/:id", async (req, res) => {
   res.send(req.playlist);
+});
+
+/** Routing middleware for GET /playlists/:id/tracks */
+router.get("/:id/tracks", async (req, res) => {
+  const tracks = await getTracksByPlaylistId(req.playlist.id);
+  res.send(tracks);
+});
+
+/** Routing middleware for POST /playlists/:id/tracks
+ * adds a new track to the playlist
+ * trackId should be sent in request body
+ * Sends the created playlist_track with status 201*/
+router.post("/:id/tracks", async (req, res) => {
+  if (!req.body) return res.status(400).send("Request body required.");
+
+  const { trackId } = req.body;
+  if (!trackId) return res.status(400).send("Request body requires: trackId");
+
+  const playlistTrack = await createPlaylistTrack(req.playlist.id, trackId);
+  res.status(201).send(playlistTrack);
 });
