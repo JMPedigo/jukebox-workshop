ca53e42a4c3856c97cb9211e2bfa65520c646ccb
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-04 17:04:06 -0500

    POST /playlists creates a new empty playlist

--- api/playlists.js
+++ api/playlists.js
@@ -3,10 +3,24 @@ const router = express.Router();
 export default router;
 
 import db from "#db/client";
-import { getPlaylists } from "#db/queries/playlists";
+import { createPlaylist, getPlaylists } from "#db/queries/playlists";
 
 /** Routing middleware for GET /playlists */
 router.get("/", async (req, res) => {
   const playlists = await getPlaylists();
   res.send(playlists);
+});
+
+/** Routing middleware for POST /playlists
+ * Creates a new empty playlist
+ */
+router.post("/", async (req, res) => {
+  if (!req.body) return res.status(400).send("Request must have a body.");
+
+  const { name, description } = req.body;
+  if (!name || !description)
+    return res.status(400).send("Request body requires: name, description");
+
+  const playlist = await createPlaylist({ name, description });
+  res.status(201).send(playlist);
 });
