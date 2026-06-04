2bc893985662ef848628eeec83e8bcc0751c0e6a
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-04 16:27:35 -0500

    Adds body parsing middleware and error handling middleware to app

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
 import express from "express";
 const app = express();
 export default app;
+
+import tracksRouter from "#api/tracks";
+/** Body-parsing middleware */
+app.use(express.json());
+
+app.use("/tracks", tracksRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Jukebox API.");
+});
+
+/** Error-handling middleware */
+app.use((err, req, res, next) => {
+  // A switch statement can be used instead of if statements
+  // when multiple cases are handled the same way.
+  switch (err.code) {
+    // Invalid type
+    case "22P02":
+      return res.status(400).send(err.message);
+    // Unique constraint violation
+    case "23505":
+    // Foreign key violation
+    case "23503":
+      return res.status(400).send(err.detail);
+    default:
+      next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Sorry! Something went wrong.");
+});
