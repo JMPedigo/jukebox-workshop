6a58c1caca7e19587c4f53c941724c603237f938
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-04 16:01:22 -0500

    Creates pathing of imports, and wraps createPlaylistTrack in braces since it is an async function, not default

--- db/seed.js
+++ db/seed.js
@@ -1,9 +1,9 @@
 import db from "#db/client";
 import { faker } from "@faker-js/faker";
 
-import { createPlaylist } from "./queries/playlist";
-import { createTrack } from "./queries/tracks";
-import createPlaylistTrack from "./queries/playlists_tracks";
+import { createPlaylist } from "#db/queries/playlist";
+import { createTrack } from "#db/queries/tracks";
+import { createPlaylistTrack } from "#db/queries/playlists_tracks";
 await db.connect();
 await seed();
 await db.end();
