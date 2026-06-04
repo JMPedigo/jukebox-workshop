b50c58abf94198d1e3615f0cf1793b2481cff38b
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-04 14:37:22 -0500

    Creates playlists_tracks in db/queries, and Adds createPlaylistTrack to database

---
+++ db/queries/playlists_tracks.js
@@ -0,0 +1,13 @@
+import db from "#db/client";
+
+export default function createPlaylistTrack(playlistId, trackId) {
+  const sql =`
+  INSERT INTO playlists_tracks
+    (playlist_id, track_id)
+  VALUES
+    ($1, $2)
+  RETURNING *
+  `;
+  const { rows: [playlistTrack], } = await db.query(sql, [playlistId, trackId]);
+  return playlistTrack;
+}
